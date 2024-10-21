a486871cf7ead09903841484244be64b0af346b9
beWiZ <38848227+downlifted@users.noreply.github.com>
2024-10-21 01:32:42 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 from .BWIZ_CaptainWebhook import BWIZ_CaptainWebhook
 from .BWIZ_AdvancedLoadImageBatch import BWIZ_AdvancedLoadImageBatch
-from .BWIZ_NotificationSound import BWIZ_NotificationSound 
+from .BWIZ_NotificationSound import BWIZ_PlaySound 
 from .BWIZ_ErrorDetector import BWIZ_ErrorDetector
 from .BWIZ_HFRepoBatchLoader import BWIZ_HFRepoBatchLoader
 
 NODE_CLASS_MAPPINGS = {
     "BWIZ_CaptainWebhook": BWIZ_CaptainWebhook,
     "BWIZ_AdvancedLoadImageBatch": BWIZ_AdvancedLoadImageBatch,
-    "BWIZ_NotificationSound": BWIZ_NotificationSound,
+    "BWIZ_NotificationSound": BWIZ_PlaySound,
     "BWIZ_ErrorDetector": BWIZ_ErrorDetector,
     "BWIZ_HFRepoBatchLoader": BWIZ_HFRepoBatchLoader,
 
